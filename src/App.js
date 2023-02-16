b289af9581d91ce980cf0736b43d09ffafbec726
Noel Zamora <noelio1290@gmail.com>
2023-02-16 13:45:41 -0600

    componente Tourcard funcionando con data json

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import './App.css';
 import TourCard from './Components/TourCard';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import SearchAppBar from './Components/AppBar';
+import cities from "./Components/Data.json"
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Grid container spacing={4}>
-          <TourCard/>
-          <TourCard/>
-          <TourCard/>
-          <TourCard/>
-        </Grid>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <>
+            <Typography
+              variant='h4'
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Conoce {city.name}!!
+            </Typography>
+          <Grid container spacing={5}>
+            {city.tours.map((tour, index) => (
+              <TourCard tour={tour} key={index} />
+              ))};
+          </Grid>
+          </>
+        ))}
+        
       </Container>
     </div>
   );
--- src/Components/TourCard.jsx
+++ src/Components/TourCard.jsx
@@ -4,18 +4,49 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import {AccessTime} from "@mui/icons-material"
 import Rating from '@mui/material/Rating';
+import {createTheme, ThemeProvider} from "@mui/material"
 
-const TourCard = ()=>{
+const theme = createTheme({
+    components: {
+        MuiTypography: {
+            variants:[
+                {
+                    props: {
+                        variant: "body2"
+                    },
+                    style: {
+                        fontSize: 11,
+                    }
+                },
+                {
+                    props: {
+                        variant: "body3"
+                    },
+                    style: {
+                        fontSize: 9,
+                    }
+                }
+            ]
+        }
+    }
+})
+
+const TourCard = ({tour})=>{
     return (
         <Grid item xs={3}>
+            <ThemeProvider theme={theme}>
             <Paper elevation={5}>
-                <img src="https://p4.wallpaperbetter.com/wallpaper/134/1001/249/singapore-city-world-beautiful-places-wallpaper-preview.jpg" 
+                <img 
+                src={tour.image}
                 alt="" 
                 className='img'
                 />
                 <Box paddingX={1}>
-                    <Typography variant="h5" component="h2">
-                        Ecatepec
+                    <Typography 
+                        variant="h5" 
+                        component="h2"
+                    >
+                        {tour.name}
                     </Typography>
                     <Box
                         sx={{
@@ -24,19 +55,55 @@ const TourCard = ()=>{
                         }}
                     >
                         <AccessTime sx={{ width: 12.5 }} />
-                        <Typography variant="body2" component="p" marginLeft={0.5} >
-                            5 hours
+                        <Typography 
+                            variant="body2" 
+                            component="p" 
+                            marginLeft={0.5} 
+                        >
+                            {tour.duration} minutos
                         </Typography>
                     </Box>
-                    <Box sx={{
+                    <Box 
+                        sx={{
                             display:"flex",
                             alignItems:"center"
                         }}
+                        marginTop={3}
                     >
-                        <Rating name="read-only" value={4.5} readOnly />
-                    </Box>  
+                        <Rating 
+                            name="read-only" 
+                            value={tour.rating} 
+                            readOnly 
+                            precision={0.5} 
+                            size="small" 
+                        />
+                        <Typography 
+                            variant="body2" 
+                            component="p" 
+                            marginLeft={0.5} 
+                        >
+                            {tour.rating} 
+                        </Typography>
+                        <Typography 
+                            variant="body3" 
+                            component="p" 
+                            marginLeft={1.5} 
+                        >
+                           ({tour.numberOfReviews} reviews)
+                        </Typography>
+                    </Box> 
+                    <Box>
+                        <Typography 
+                            variant="h6" 
+                            component="h3" 
+                            marginTop={0} 
+                        >
+                            Desde ${tour.price} Pesitos 
+                        </Typography>
+                    </Box> 
                 </Box>
             </Paper>
+            </ThemeProvider>
         </Grid>
     );
 };
