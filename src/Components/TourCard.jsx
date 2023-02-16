76df9543c7ae80f277e2de073a627f949cc1a3d9
Noel Zamora <noelio1290@gmail.com>
2023-02-16 11:34:18 -0600

    componente estrellas de ranking agregado

--- src/Components/TourCard.jsx
+++ src/Components/TourCard.jsx
@@ -2,6 +2,8 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import {AccessTime} from "@mui/icons-material"
+import Rating from '@mui/material/Rating';
 
 const TourCard = ()=>{
     return (
@@ -21,10 +23,18 @@ const TourCard = ()=>{
                             alignItems:"center"
                         }}
                     >
-                        <Typography variant="body2" component="p">
+                        <AccessTime sx={{ width: 12.5 }} />
+                        <Typography variant="body2" component="p" marginLeft={0.5} >
                             5 hours
                         </Typography>
                     </Box>
+                    <Box sx={{
+                            display:"flex",
+                            alignItems:"center"
+                        }}
+                    >
+                        <Rating name="read-only" value={4.5} readOnly />
+                    </Box>  
                 </Box>
             </Paper>
         </Grid>
